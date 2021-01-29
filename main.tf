7ff4d177bef0b82acc2979b04669c92da9137348
Çetin ARDAL <kral2@users.noreply.github.com>
2021-01-29 02:17:30 +0100

    remove provider block - not needed with RM

--- main.tf
+++ main.tf
@@ -17,13 +17,13 @@ terraform {
   }
 }
 
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.homeregion
-}
+# provider "oci" {
+#   tenancy_ocid     = var.tenancy_ocid
+#   user_ocid        = var.user_ocid
+#   fingerprint      = var.fingerprint
+#   private_key_path = var.private_key_path
+#   region           = var.homeregion
+# }
 
 /*
 * This example shows how to create a compartement and two sub-compartemnt.
